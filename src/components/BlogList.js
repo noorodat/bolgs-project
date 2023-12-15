5883e625e7f2ea235625a32d8b00d7600fcae39f
B1UG <noor.feraas@gmail.com>
2023-12-16 01:50:44 +0300

    fetch data + delete blog

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,19 +1,20 @@
 
 
-
-
-const BlogList = (data) => {
-
-    const blogs = data.blogs;
+const BlogList = ({blogs, title, handleDelete}) => {
 
     return (
         <div className="blog-list">
+            <h2>{title}</h2>
             {
                 blogs.map((blog) => (
                     <div className="blog-preview" key={blog.id}>
                         <h2>{blog.title}</h2>
                         <p>By : {blog.author}</p>
                         <p>{blog.body}</p>
+                        <div className="btns" style={{display: "flex", gap: "10px", justifyContent: 'center', padding: "10px"}}>
+                        <button onClick={() => handleDelete(blog.id)}>Delete</button>
+                        <button>Edit</button>
+                        </div>
                     </div>
                 ))
             }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,50 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import axios from "axios";
 
 
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body : 'idk idk idk', author: 'Nooraldeen', id: 1},
-        {title: 'Welcome buddy', body : 'idk idk idk', author: 'Bashar', id: 2},
-        {title: 'New cool feature', body : 'idk idk idk', author: 'Omar', id: 3},
-    ]);
+    const [blogs, setBlogs] = useState([]);
+
+    const [name, setName] = useState('Noor');
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleDelete = (id) => {
+        axios.delete(`http://localhost:8000/blogs/${id}`)
+            .then(() => {
+                return axios.get('http://localhost:8000/blogs');
+            })
+            .then(res => {
+                setBlogs(res.data);
+                setIsLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    // It runs once the pages loads (initial render)
+    useEffect(() => {
+        axios.get('http://localhost:8000/blogs')
+            .then(res => {
+                setBlogs(res.data);
+                setIsLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs"/>
+            {isLoading && <div className="loading">Loading...</div>}
+            <h1>{name}</h1>
+            {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />}
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Omar')} title="Omar's Blog"/> */}
+            <button onClick={() => setName('Nooraldeen')}>Change name</button>
         </div>
     );
 }
